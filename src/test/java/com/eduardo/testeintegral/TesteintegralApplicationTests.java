19168e504c76122d47b0ecfb114196fb9358c474
Eduardo <edu.goes10@gmail.com>
2026-03-31 15:08:47 -0300

    first commit

---
+++ src/test/java/com/eduardo/testeintegral/TesteintegralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.testeintegral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteintegralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
